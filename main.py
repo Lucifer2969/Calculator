9f633747e9d5e170c6cc165275a66ff040ad4983
Raghvendra Singh <raghv.afgji@icloud.com>
2025-01-11 19:28:27 +0530

    Main changes done.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+import art
 
+print(art.cal_design)
 
-
-
-
-
-
-
+condition = True
+first_num = float(input("Enter the first number: "))
 
 def add(n1, n2):
     return n1 + n2
@@ -26,4 +24,26 @@ func_dictionary = {
     '*': multiply,
 }
 
-print(func_dictionary['*'](3,4))
+
+while condition:
+    print('''Select any one operation.
+ *
+ +
+ -
+ / ''')
+    operation = input("Enter your choice:- ")
+    second_num = float(input("Enter the next number: "))
+
+    for key in func_dictionary:
+        if operation == key:
+            value = func_dictionary[key](first_num,second_num)
+            operation_symbol = key
+    print(f"{first_num} {operation_symbol} {second_num} = {value}")
+    cont_condition = input("Enter 'y' if you want to continue with perivious output.\
+Enter 'n' if you want to start new calculation. \n")
+    if cont_condition == 'y':
+        first_num = value
+    elif cont_condition == 'n':
+        print('\n'*100)
+        print(art.cal_design)
+        first_num = float(input("Enter the first number: "))
