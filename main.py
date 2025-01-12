a1967bee5a5ab3520728020088b15a9e39946824
Raghvendra Singh <raghv.afgji@icloud.com>
2025-01-12 11:08:33 +0530

    Final code changes done

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import art
 
-print(art.cal_design)
-
 condition = True
-first_num = float(input("Enter the first number: "))
 
 def add(n1, n2):
     return n1 + n2
@@ -24,26 +21,22 @@ func_dictionary = {
     '*': multiply,
 }
 
+def calculator():
+    print(art.cal_design)
+    first_num = float(input("Enter the first number: "))
+    while condition:
+        print('Select your operation')
+        for key in func_dictionary:
+            print(key)
+        operation = input("Enter your choice: ")
+        second_num = float(input("Enter the next number: "))
+        value = func_dictionary[operation](first_num,second_num)
+        print(f"{first_num} {operation} {second_num} = {value}")
+        cont_condition = input("Enter 'y' if you want to continue with perivious output. Enter 'n' if you want to start new calculation. \n")
+        if cont_condition == 'y':
+            first_num = value
+        elif cont_condition == 'n':
+            print('\n'*100)
+            calculator()
 
-while condition:
-    print('''Select any one operation.
- *
- +
- -
- / ''')
-    operation = input("Enter your choice:- ")
-    second_num = float(input("Enter the next number: "))
-
-    for key in func_dictionary:
-        if operation == key:
-            value = func_dictionary[key](first_num,second_num)
-            operation_symbol = key
-    print(f"{first_num} {operation_symbol} {second_num} = {value}")
-    cont_condition = input("Enter 'y' if you want to continue with perivious output.\
-Enter 'n' if you want to start new calculation. \n")
-    if cont_condition == 'y':
-        first_num = value
-    elif cont_condition == 'n':
-        print('\n'*100)
-        print(art.cal_design)
-        first_num = float(input("Enter the first number: "))
+calculator()
